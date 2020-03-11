c9f60fa666f572ea0c63a1d5257dd1e0098f0885
csant510 <james.sant83@gmail.com>
2020-03-11 10:29:56 -0700

    cleaning up commits

---
+++ build/precache-manifest.c512d5f620a5178fe1d57cfdf0e36abd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5c77d690daee9e10626c953bab35a0",
+    "url": "/github.io/csant510/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c8f13a9f772d9736e341",
+    "url": "/github.io/csant510/monsters-rolodex/static/css/main.f1fe948d.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/github.io/csant510/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github.io/csant510/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f13a9f772d9736e341",
+    "url": "/github.io/csant510/monsters-rolodex/static/js/main.09b68d15.chunk.js"
+  },
+  {
+    "revision": "6de8937592c754fcabed",
+    "url": "/github.io/csant510/monsters-rolodex/static/js/runtime-main.71caad81.js"
+  }
+]);
